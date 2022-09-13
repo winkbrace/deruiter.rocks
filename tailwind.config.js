091a9ee3f8e0e3624e100c024cc3d8202a07127f
Bas de Ruiter <bas@basbv.it>
2022-09-13 20:52:41 +0200

    Build first draft of Gerda page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./gerda/**/*.{html,js}"],
+    theme: {
+        fontFamily: {
+            sans: ['Nanum Gothic', 'sans-serif'],
+            'rocks': ['Bangers', 'cursive'],
+        },
+        extend: {},
+    },
+    plugins: [],
+}
